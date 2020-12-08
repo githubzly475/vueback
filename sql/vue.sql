e26bfb7874564c5342da30e7e832626f2b8fe4cc
gaowenxiang <wx4gao@163.com>
2020-12-08 15:04:17 +0800

    version temp

--- sql/vue.sql
+++ sql/vue.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50635
-Source Host           : localhost:3306
+Source Server         : 192.168.0.99_3306
+Source Server Version : 50728
+Source Host           : 192.168.0.99:3306
 Source Database       : vue
 
 Target Server Type    : MYSQL
-Target Server Version : 50635
+Target Server Version : 50728
 File Encoding         : 65001
 
-Date: 2020-12-01 14:44:11
+Date: 2020-12-08 14:56:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,6 +33,11 @@ CREATE TABLE `sys_dept` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='部门';
 
 -- ----------------------------
+-- Records of sys_dept
+-- ----------------------------
+INSERT INTO `sys_dept` VALUES ('1', '1', '1', '1', '0', '2020-12-02 13:14:17', '2020-12-02 13:14:21', '1', '1');
+
+-- ----------------------------
 -- Table structure for sys_dict_key
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_dict_key`;
@@ -41,13 +46,19 @@ CREATE TABLE `sys_dict_key` (
   `key_name` varchar(50) DEFAULT NULL COMMENT '名称',
   `key_code` varchar(50) DEFAULT NULL COMMENT '标识',
   `key_remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  `del_flag` int(11) DEFAULT NULL COMMENT '删除标识0-正常，1-删除',
+  `del_flag` int(11) DEFAULT '0' COMMENT '删除标识0-正常，1-删除',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='字典表-键表';
+
+-- ----------------------------
+-- Records of sys_dict_key
+-- ----------------------------
+INSERT INTO `sys_dict_key` VALUES ('1', '中文', 'zh', '语言', '0', '2020-12-02 13:14:30', '2020-12-02 13:14:33', '1', '1');
+INSERT INTO `sys_dict_key` VALUES ('a59ec8346e1a49c8aa8c1c6ef5284527', '测试', 'TEST', '测试', '0', '2020-12-08 10:58:54', '2020-12-08 10:59:16', '1', '1');
 
 -- ----------------------------
 -- Table structure for sys_dict_value
@@ -61,13 +72,23 @@ CREATE TABLE `sys_dict_value` (
   `value_sort` int(11) DEFAULT NULL COMMENT '排序',
   `parent_id` varchar(32) DEFAULT NULL COMMENT '父Id',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  `del_flag` int(11) DEFAULT NULL COMMENT '0-正常 1-删除',
+  `del_flag` int(11) DEFAULT '0' COMMENT '0-正常 1-删除',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='字典表-值表';
+
+-- ----------------------------
+-- Records of sys_dict_value
+-- ----------------------------
+INSERT INTO `sys_dict_value` VALUES ('1', '中文简体', 'zh-CHS', '1', '1', '0', '中文简体', '0', '2020-12-02 13:14:44', '2020-12-02 13:14:46', '1', '1');
+INSERT INTO `sys_dict_value` VALUES ('2', '中文繁体', 'zh-CHT', '1', '2', '0', '中文繁体', '0', '2020-12-08 09:19:23', '2020-12-08 09:19:27', '1', '1');
+INSERT INTO `sys_dict_value` VALUES ('3', '测试一', 'test-1', 'a59ec8346e1a49c8aa8c1c6ef5284527', '1', '0', '测试一', '0', null, '2020-12-08 13:44:33', '1', '1');
+INSERT INTO `sys_dict_value` VALUES ('50ef79da1bda4a98a42ddaf20e7fe7f9', '测试三', 'test-3', 'a59ec8346e1a49c8aa8c1c6ef5284527', '3', '0', '测试三', '0', '2020-12-08 14:31:17', '2020-12-08 14:31:39', '1', '1');
+INSERT INTO `sys_dict_value` VALUES ('5eaf634d4ae442f7a6890b6a00800bc8', '测试四', 'test-4', 'a59ec8346e1a49c8aa8c1c6ef5284527', '4', null, '测试四', '0', '2020-12-08 14:32:55', null, '1', null);
+INSERT INTO `sys_dict_value` VALUES ('d1848715287c40368753b4bcc4b47e9b', '测试二', 'test-2', 'a59ec8346e1a49c8aa8c1c6ef5284527', '2', '0', '测试二', '0', '2020-12-08 13:45:10', null, '1', null);
 
 -- ----------------------------
 -- Table structure for sys_log
@@ -94,6 +115,10 @@ CREATE TABLE `sys_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统日志';
 
 -- ----------------------------
+-- Records of sys_log
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for sys_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
@@ -111,9 +136,26 @@ CREATE TABLE `sys_menu` (
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新人',
-  `del_flag` int(11) DEFAULT NULL COMMENT '0正常 1删除',
-  PRIMARY KEY (`id`)
+  `del_flag` int(11) DEFAULT '0' COMMENT '0正常 1删除',
+  PRIMARY KEY (`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES ('051de277cc734eebb74e797dc5b2e225', '图标管理', '', 'icon', '', '0', 'picture', '0', '0', '2020-12-03 15:17:45', null, '1', null, '0');
+INSERT INTO `sys_menu` VALUES ('064f5b93cc8147249509094721e51ee5', '字典管理', 'sys:directory', 'dictionary', 'dict', '1', 'search', '7', '1', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('1', '系统管理', null, 'system', null, '0', 'aboutMe', '0', '0', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('10', '菜单新增', 'sys:menu:add', null, null, '16', '', '0', '2', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('11', '部门新增', 'sys:dept:add', null, null, '3', '', '0', '2', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('13', '角色管理', 'sys:role', 'role', 'role', '1', 'role', '2', '1', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('16', '菜单管理', 'sys:menu', 'menu', 'menu', '1', 'menu', '9999', '1', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('2', '用户管理', 'sys:user', 'user', 'user', '1', 'user', '1', '1', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('3', '部门管理', 'sys:dept', 'dept', 'dict/DeptTempIndex', '1', 'dept', '3', '1', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('3dc9d7b362c6410d8ad157fd78ace458', '用户列表', 'sys:user:list', null, null, '2', 'list', '0', '2', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('47ac744b4c7745eeb3696f7419ec9dc7', '图标管理', 'sys:icon', '/icon', 'svgIcons', '051de277cc734eebb74e797dc5b2e225', 'icon', '0', '1', '2020-12-03 15:27:32', null, '1', null, '0');
+INSERT INTO `sys_menu` VALUES ('5', '用户新增', 'sys:user:add', null, null, '2', 'authority', '0', '2', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
+INSERT INTO `sys_menu` VALUES ('7cdbaeec1bd64d2a9dd47b4f238646c7', '菜单删除', 'sys:menu:delete', null, null, '16', null, '0', '2', '2020-12-03 08:25:09', '2020-12-03 08:25:09', '1', '1', '0');
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -131,8 +173,13 @@ CREATE TABLE `sys_role` (
   `data_scope` int(11) DEFAULT NULL COMMENT '数据权限范围 1 全部 2 本级 3 本级以及子级 4 自定义',
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新人',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', '1', '1', '1', '0', '2020-12-02 13:13:42', '2020-12-02 13:13:46', '1', '1', '1', '1');
 
 -- ----------------------------
 -- Table structure for sys_role_dept
@@ -146,6 +193,11 @@ CREATE TABLE `sys_role_dept` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色部门';
 
 -- ----------------------------
+-- Records of sys_role_dept
+-- ----------------------------
+INSERT INTO `sys_role_dept` VALUES ('1', '1', '1');
+
+-- ----------------------------
 -- Table structure for sys_role_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_menu`;
@@ -155,6 +207,25 @@ CREATE TABLE `sys_role_menu` (
   `menu_id` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统表-角色菜单表';
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES ('04345f1e285f4c16b01d4969dfefe8b0', '1', '064f5b93cc8147249509094721e51ee5');
+INSERT INTO `sys_role_menu` VALUES ('051de277cc734eebb74e797dc5b2e224', '1', '051de277cc734eebb74e797dc5b2e225');
+INSERT INTO `sys_role_menu` VALUES ('1', '1', '1');
+INSERT INTO `sys_role_menu` VALUES ('1aeb63f607da497ab88af6a9d4866e25', '1', '10');
+INSERT INTO `sys_role_menu` VALUES ('2', '1', '11');
+INSERT INTO `sys_role_menu` VALUES ('2083e668e25b49fc8320ce59149036b3', '1', '13');
+INSERT INTO `sys_role_menu` VALUES ('2ac9a13ab3a548598b6ca2b2d76ebb6f', '1', '16');
+INSERT INTO `sys_role_menu` VALUES ('2ca5c717a39d44d692dac81bdd49bc33', '1', '2');
+INSERT INTO `sys_role_menu` VALUES ('2ee82ca8d4d04c0fb738a4b9b3b06bfc', '1', '3');
+INSERT INTO `sys_role_menu` VALUES ('3051a20b41a44a6f95ca1b46deb9cffb', '1', '3dc9d7b362c6410d8ad157fd78ace458');
+INSERT INTO `sys_role_menu` VALUES ('3f65b78ce47b47bbaf6107886d6bfe5c', '1', '42e260af7a364d0dab15f606f4fd3fcb');
+INSERT INTO `sys_role_menu` VALUES ('3f798cfad4554be78327219cee471409', '1', '5');
+INSERT INTO `sys_role_menu` VALUES ('4618d137952246969348212fde5c7685', '1', '7cdbaeec1bd64d2a9dd47b4f238646c7');
+INSERT INTO `sys_role_menu` VALUES ('47ac744b4c7745eeb3696f7419ec9dc6', '1', '47ac744b4c7745eeb3696f7419ec9dc7');
+INSERT INTO `sys_role_menu` VALUES ('48082c22ac1741819ed997253f72ff49', '1', 'e96e6c48828f4e75ad56b55d7c5a9255');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -182,8 +253,13 @@ CREATE TABLE `sys_user` (
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新人',
   `openid` varchar(255) DEFAULT NULL COMMENT 'openid',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户';
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', '$2a$10$6piAZo7djNYIcpD2dOmNS.pifvkWyrd43b0paRhSR0O/YtaPUwMX.', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '2020-12-02 10:33:38', '2020-12-02 10:33:47', '1', '1', '1');
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -195,3 +271,8 @@ CREATE TABLE `sys_user_role` (
   `role_id` varchar(32) DEFAULT NULL COMMENT '角色ID',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统用户角色';
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('1', '1', '1');
